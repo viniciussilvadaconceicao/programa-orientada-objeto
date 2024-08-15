b52003d72187d7c08cc1b457733ea650cdccc6c6
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-15 20:22:39 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,2 +1,11 @@
-#init é o metodo construtor da classe
+#init é o metodo construtor da classe 
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def imprimir(self):
+        print('Nome:', self.nome)
+        print('Idade:', self.idade)
+
 #self é o objeto que está sendo criado

23b1302fbd8d2f5b4852a89745b2fa8fe21c1ac1
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-15 21:02:10 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -4,7 +4,19 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
 
-    def imprimir(self):
-        print('Nome:', self.nome)
-        print('Idade:', self.idade)
+    def agradar_o_outro(self):
+        var = input("fale algo agradavel: ")
+        if var == "oi":
+            print(f"{self.nome} disse que você esta muito bem hoje fulano")
+        else:
+            print(f"{self.nome} não é uma frasede finida")
+            
+    def desagradar_o_outro(self):
+        var = input("fale algo desagradavel: ")
+        if var == "não" or var == "NÃO":
+            print(f"{self.nome} você esta de mal humor hoje")
+        else:
+            print(f"{self.nome} não é uma frase definida")
+
 #self é o objeto que está sendo criado
+#self.nome é o atributo do objeto

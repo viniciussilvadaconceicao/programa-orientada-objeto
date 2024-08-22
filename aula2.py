eba96de813ebf3b82fe7db49ca7af39430ee778f
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-22 19:50:48 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,2 +1,14 @@
 class carro :
-    def __init__(aelf, marca, modelo, cor, ) 
+    def __init__(self, marca, modelo, cor, cambio, ano):
+        self.cambio = cambio
+        self.ano = ano 
+        self.cor = cor
+        self.modelo = modelo
+        self.marca = marca
+    
+    def apresentar():
+        '''aqui ira apresentar'''
+    
+carro1 = carro('bmw', 'x6', 'azul', 'automatico', '2024')
+
+    

f6b64a07fac8024d91b04a2d6d187069d3ba713a
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-22 20:15:32 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -5,16 +5,19 @@ class carro :
         self.cor = cor
         self.modelo = modelo
         self.marca = marca
-    
+
+
     def apresentar(self):
         '''aqui ira apresentar'''
-        print(f'''
+        print(
+        f'''
     o carro é :
     marca:{self.marca}
     modelo:{self.modelo}
     cor:{self.cor}
     cambio:{self.cambio}
     ano;{self.ano}
-''')
+        ''')
     
 carro1 = carro('bmw', 'x6', 'azul', 'automatico', '2024')
+carro1.apresentar()

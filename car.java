73d2cd8ef962fa2393c960bddac105167e8d3efe
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-08-29 19:39:39 -0300

    Update car.java

--- car.java
+++ car.java
@@ -4,5 +4,18 @@ public class Car{
     int year;
     boolean isRunning;
     int velocidade ;
-    
+
+    public Car(string marca, string modelo, int year){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.year = year;
+        this.lsRunning = false;
+        this.velocidade = 0;
+    }
+    // definindo os metodos
+    public void start(){
+        if (lisRunning){
+            lsRunning = 
+        }
+    }
 }
